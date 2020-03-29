c1dad1aa5500d0343f5fc84c1a1d144194ecb6ad
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-03-29 14:21:53 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
+import GlobalStyles from './styles/global';
 
 import './config/ReactotronConfig';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyles />
       <Routes />
     </Router>
   );
---
+++ src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    *:focus {
+        outline: 0;
+    }
+
+    body, html, #root {
+        height: 100%;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button {
+        font: 14px 'Roboto', sans-serif;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    button {
+        cursor: pointer;
+    }
+`;
