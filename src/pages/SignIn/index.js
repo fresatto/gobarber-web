f8ab5df3dbafffdfaaf8493fd61522c5ff3e88ab
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-01 00:54:52 -0300

    loading na autenticaçao'

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -16,6 +16,7 @@ export default function SignIn() {
   });
 
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -26,7 +27,7 @@ export default function SignIn() {
         <img src={logo} alt="GoBarber Llogo" />
         <Input name="email" type="text" placeholder="Digite seu e-mail" />
         <Input name="password" type="password" placeholder="Digite sua senha" />
-        <button type="submit">Acessar</button>
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,12 +3,11 @@ import { all, takeLatest, put, call } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 function* signIn({ payload }) {
-  const { email, password } = payload;
-
   try {
+    const { email, password } = payload;
     const response = yield call(api.post, '/sessions', {
       email,
       password,
@@ -24,7 +23,7 @@ function* signIn({ payload }) {
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (error) {
-    console.tron.error('User not found');
+    yield put(signFailure());
   }
 }
 
