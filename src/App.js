19a5e60ec5739976f1b18fbb9200d838b2d877a7
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-01 00:40:05 -0300

    redux-persist

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import history from './services/history';
 import GlobalStyles from './styles/global';
 import './config/ReactotronConfig';
-import store from './store';
+import { store, persistor } from './store';
 import Routes from './routes';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <GlobalStyles />
-        <Routes />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyles />
+          <Routes />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
