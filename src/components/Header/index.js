23ef5a35ee05a6e3aea887dbffaefa864fcd4565
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-01 21:55:19 -0300

    Estilo do header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/images/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <Link to="/dashboard">
+          <nav>
+            <img src={logo} alt="Logo roxo" />
+            <h3>DASHBOARD</h3>
+          </nav>
+        </Link>
+        <aside>
+          <Profile>
+            <div>
+              <h3>Gabriel Fresatto</h3>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/45/abott@adorable.png"
+              alt="Avatar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+`;
+
+export const Content = styled.div`
+  max-width: 900px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  padding: 0 20px;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+
+    h3 {
+      color: #7159c1;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    margin-left: 20px;
+    padding-left: 20px;
+    border-left: 1px solid #eee;
+
+    h3 {
+      font-size: 15px;
+    }
+
+    a {
+      text-align: right;
+      display: block;
+      font-size: 13px;
+      color: #999;
+    }
+  }
+
+  img {
+    border-radius: 50%;
+    height: 34px;
+    margin-left: 10px;
+  }
+`;
