062eba8d0196e98e43d12a3741a780e2be779a5f
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-04 17:21:51 -0300

    Estilos do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#FFF" size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight color="#FFF" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Paunocu</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego Paunocu</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego Paunocu</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
