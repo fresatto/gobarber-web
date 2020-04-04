0e28e6c8b5455b349834d1f3ffcc80cc12807d11
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-04 14:53:08 -0300

    Estilizaçao da pagina de profile

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,36 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Form, Input } from '@rocketseat/unform';
-import { Container } from './styles';
+
+// import { Container } from './styles';
 
 export default function Dashboard() {
-  const profile = useSelector((state) => state.user.profile);
-
-  function handleUpdateUser(data) {}
-
-  return (
-    <Container>
-      <Form initialData={profile} onSubmit={handleUpdateUser}>
-        <Input name="name" placeholder="Seu nome de usuário" />
-        <Input name="email" type="email" placeholder="Seu nome de usuário" />
-
-        <hr />
-
-        <Input
-          type="password"
-          name="oldPassowrd"
-          placeholder="Sua senha atual"
-        />
-        <Input type="password" name="password" placeholder="Nova senha" />
-        <Input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirmação de senha"
-        />
-
-        <button type="submit">Atualizar perfil</button>
-      </Form>
-      <button type="button">Sair do GoBarber</button>
-    </Container>
-  );
+  return <div />;
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleUpdateUser(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleUpdateUser}>
+        <Input name="name" placeholder="Seu nome de usuário" />
+        <Input name="email" type="email" placeholder="Seu nome de usuário" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassowrd"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
