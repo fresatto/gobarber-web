84cc9797f50a667cf0abcb1abba8e61447414c4d
Gabriel Fresatto <gabriel.fresatto@juni.com.br>
2020-04-04 16:57:19 -0300

    Header com informaçoes do redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Container, Content, Profile } from './styles';
@@ -6,6 +7,7 @@ import logo from '~/assets/images/logo-purple.svg';
 import Notifications from '../Notifications';
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
   return (
     <Container>
       <Content>
@@ -19,11 +21,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <h3>Gabriel Fresatto</h3>
+              <h3>{profile.name}</h3>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/45/abott@adorable.png"
+              src={
+                profile.avatar
+                  ? profile.avatar.url
+                  : 'https://api.adorable.io/avatars/45/abott@adorable.png'
+              }
               alt="Avatar"
             />
           </Profile>
